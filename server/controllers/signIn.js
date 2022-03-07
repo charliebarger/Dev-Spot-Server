49ea34147e3de546eddabf22ed7d5cf54569c907
charliebarger <charliebarger04@gmail.com>
2022-03-07 16:27:37 -0700

    move to controller

--- server/controllers/signIn.js
+++ server/controllers/signIn.js
@@ -4,18 +4,19 @@ const jwt = require("jsonwebtoken");
 const passport = require("passport");
 /* POST login. */
 
-exports.logIn = (req, res, next) => {
+exports.logIn = (req, res) => {
   passport.authenticate("local", { session: false }, (err, user) => {
     if (err || !user) {
-      return res.status(401).json({
+      return res.status(400).json({
         message: "Something is not right",
+        user: user,
       });
     }
     req.login(user, { session: false }, (err) => {
       if (err) {
         res.send(err);
       }
-      const token = jwt.sign(user, "secret");
+      const token = jwt.sign(user.toJSON(), "secret");
       return res.json({ user, token });
     });
   })(req, res);
--- server/routes/users.js
+++ server/routes/users.js
@@ -17,23 +17,7 @@ const passport = require("passport");
 const { user } = require("../pop");
 
 /* POST login. */
-router.post("/login", function (req, res) {
-  passport.authenticate("local", { session: false }, (err, user) => {
-    if (err || !user) {
-      return res.status(400).json({
-        message: "Something is not right",
-        user: user,
-      });
-    }
-    req.login(user, { session: false }, (err) => {
-      if (err) {
-        res.send(err);
-      }
-      const token = jwt.sign(user.toJSON(), "secret");
-      return res.json({ user, token });
-    });
-  })(req, res);
-});
+router.post("/login", signInController.logIn);
 
 router.get(
   "/protected",
